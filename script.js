38037646da618130b5018b3a3070dcae853437b8
Haley <texashandmaids@hotmail.com>
2013-11-28 22:03:59 -0600

    jquery naming and offsets

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-var s; // start point element
-var e; // end point element
+var $s; // start point element
+var $e; // end point element
+var $d; // dot -- temporary var $for testing purposes
 
-var t; // total time var
-var tb; // time before
-var ta; // time after
+var $t; // total time var
+var $tb; // time before
+var $ta; // time after
 
-var d; // distance
-var dx; // x distance
-var dy; // y distance
+var $d; // distance
+var $dx; // x distance
+var $dy; // y distance
 
-var h; // height
-var w; // width
+var $h; // height
+var $w; // width
 
-var xs; // x start location
-var xe; // x end location
-var ys; // y start location
-var ye; // y end location
+var $xs = $s.offset().left; // x start location
+var $xe = $e.offset().left; // x end location
+var $ys = $s.offset().top; // y start location
+var $ye = $e.offset().top; // y end location
+
+
+
+
