998c3728f1451ee9fc284beecf0cdbb13c135748
Haley <texashandmaids@hotmail.com>
2013-11-28 21:57:22 -0600

    setting up variables that I think I may need
    rough draft edition: efficiency isn't currently something I'm worried
    about

---
+++ script.js
@@ -0,0 +1,18 @@
+var s; // start point element
+var e; // end point element
+
+var t; // total time var
+var tb; // time before
+var ta; // time after
+
+var d; // distance
+var dx; // x distance
+var dy; // y distance
+
+var h; // height
+var w; // width
+
+var xs; // x start location
+var xe; // x end location
+var ys; // y start location
+var ye; // y end location
