11ff41815c3ad7c6c08f9ddfd2785477c14fb1ef
Haley <texashandmaids@hotmail.com>
2013-11-29 09:01:38 -0600

    more setup - variables

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
-var $s; // start point element
-var $e; // end point element
-var $d; // dot -- temporary var $for testing purposes
+$(document).ready(function() {
 
-var $t; // total time var
-var $tb; // time before
-var $ta; // time after
+	var $w = $(window).width(); // width
+	var $h = $(window).height(); // height
 
-var $d; // distance
-var $dx; // x distance
-var $dy; // y distance
+	var $s = $('#point-one'); // start point element
+	var $e = $('#point-two'); // end point element
+	var $dot = $('.dot'); // dot -- temporary var $for testing purposes
+	var $dId = $dot.attr('id'); // dot id
 
-var $h; // height
-var $w; // width
+	var $xs = Math.round($s.offset().left); // x start location
+	var $xe = Math.round($e.offset().left); // x end location
+	var $ys = Math.round($s.offset().top); // y start location
+	var $ye = Math.round($e.offset().top); // y end location
 
-var $xs = $s.offset().left; // x start location
-var $xe = $e.offset().left; // x end location
-var $ys = $s.offset().top; // y start location
-var $ye = $e.offset().top; // y end location
+	var $dx = Math.abs($xs - $xe); // x distance
+	var $dy = Math.abs($ys - $ye); // y distance
+	var $d = Math.round(Math.sqrt(Math.pow($dx, 2) + Math.pow($dy, 2))); // distance
+
+	var $t = 700; // total time var
+	var $tb = $t * 0.8; // time before
+	var $ta = $t - $tb; // time after
+
+	console.log('Starting point is (' + $xs + ',' + $ys + ')');
+	console.log('Ending point is (' + $xe + ',' + $ye + ')');
+	console.log($dx);
+	console.log($dy);
+	console.log($d);
+});
+
 
 
 
